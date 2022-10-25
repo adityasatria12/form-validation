5112a09ed931bc6bab4899c3b9799540e5de04b2
Naufal Nur Halim <106175580+NopalHalim@users.noreply.github.com>
2022-10-25 22:25:08 +0700

    Menambahkan komentar di js

--- Responsive-Login-Form-master/js/main.js
+++ Responsive-Login-Form-master/js/main.js
@@ -1,25 +1,29 @@
-const inputs = document.querySelectorAll(".input");  // 
-const show  = document.querySelector('.show-pass')
-const hide = document.querySelector('.hide-pass')
+const inputs = document.querySelectorAll(".input");  //membuat variabel inputs yang isinya input username dan password
+const show  = document.querySelector('.show-pass')	//membuat variabel show yang isinya icon show password 
+const hide = document.querySelector('.hide-pass')	//membuat variabel hide yang isinya icon hide password
 
-// membuat passwor dapat terlihat
+// membuat fungsi agar password dapat terlihat
 show.addEventListener('click', () => {
 	inputs[1].type = 'text'
 	show.style.display = 'none'
 	hide.style.display = 'inline'
 })
 
+// membuat fungsi agar password tidak terlihat
 hide.addEventListener('click', () => {
 	inputs[1].type = 'password'
 	show.style.display = 'inline'
 	hide.style.display = 'none'
 })
 
+//Membuat fungsi menambahkan class focus pada variabel parent agar nama username/password nya keatas
 function addcl(){
 	let parent = this.parentNode.parentNode;
 	parent.classList.add("focus");
 }
 
+
+//Membuat fungsi menghapus class focus agar ketika di klik selain bagian formulir menghilang effect fungsi addcl
 function remcl(){
 	let parent = this.parentNode.parentNode;
 	if(this.value == ""){
@@ -27,7 +31,7 @@ function remcl(){
 	}
 }
 
-
+//  Memanggil fungsi masing masing yang sudah di buat di atas
 inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
